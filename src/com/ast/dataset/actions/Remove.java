a90b3f0ce04e3c3f667c1d5454811fd1e6a3f3a6
sergi <sergi@toda>
2014-03-03 16:52:23 +0100

    Awful if-else removed using dynamic binding

--- src/com/ast/dataset/actions/Remove.java
+++ src/com/ast/dataset/actions/Remove.java
@@ -1,26 +1,38 @@
 package com.ast.dataset.actions;
 
+import java.io.IOException;
+
+import com.ast.dataset.executor.DatasetExecutor;
+import com.ast.dataset.util.Config;
+import com.ast.dataset.util.FilesOp;
+
 public class Remove extends Action {
-    
-    private String filePath;
-    
-    public Remove(int secondToExecute, String filePath) {
-        super(secondToExecute);
-        this.filePath = filePath;
-    }
 
-    public String getFilePath( ) {
-        return filePath;
-    }
+	private String filePath;
 
-    public void setFilePath( String filePath ) {
-        this.filePath = filePath;
-    }
-    
-    @Override
-    public String toString(){
-        String str = this.getSecondToExecute()+" REMOVE "+filePath+"\n";
-        return str;
-    }
+	public Remove(int secondToExecute, String filePath) {
+		super(secondToExecute);
+		this.filePath = filePath;
+	}
+
+	@Override
+	public void performAction(DatasetExecutor executor) throws IOException {
+		String removePath = Config.getFolderPath() + this.getFilePath();
+		FilesOp.removeFile(removePath);
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	@Override
+	public String toString() {
+		String str = this.getSecondToExecute() + " REMOVE " + filePath + "\n";
+		return str;
+	}
 
 }
--- src/com/ast/dataset/actions/Update.java
+++ src/com/ast/dataset/actions/Update.java
@@ -1,27 +1,60 @@
 package com.ast.dataset.actions;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
+import org.apache.log4j.Logger;
+
+import com.ast.dataset.executor.DatasetExecutor;
+
 public class Update extends Action {
-    
-    private String filePath;
-    ArrayList<ByteRange> modifications;
-    
-    public Update(int secondToExecute, String filePath, ArrayList<ByteRange> modifications) {
-        super(secondToExecute);
-        this.filePath = filePath;
-        this.modifications = modifications;
-    }
 
-    public String getFilePath( ) {
-        return filePath;
-    }
+	private Logger logger = Logger.getLogger(Update.class.getName());
 
-    public void setFilePath( String filePath ) {
-        this.filePath = filePath;
-    }
-    
-    public ArrayList<ByteRange> getModifications() {
+	private String filePath;
+	ArrayList<ByteRange> modifications;
+
+	public Update(int secondToExecute, String filePath, ArrayList<ByteRange> modifications) {
+		super(secondToExecute);
+		this.filePath = filePath;
+		this.modifications = modifications;
+	}
+
+	@Override
+	public void performAction(DatasetExecutor executor) throws IOException {
+		logger.debug("Perform an update action.");
+
+		String filePath = this.getFilePath();
+		filePath = filePath.substring(1);
+		RandomAccessFile fileStream = new RandomAccessFile(executor.getRootPath() + "/" + filePath, "rw");
+
+		ArrayList<ByteRange> modifications = this.getModifications();
+
+		for (ByteRange modification : modifications) {
+			// Change from KB to B
+			int dataSize = (modification.getByteEnd() - modification.getByteStart()) * 1024;
+			byte data[] = new byte[dataSize];
+
+			executor.getRandom().nextBytes(data);
+
+			int byteStart = modification.getByteStart() * 1024;
+			fileStream.seek(byteStart);
+			fileStream.write(data);
+		}
+
+		fileStream.close();
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public ArrayList<ByteRange> getModifications() {
 		return modifications;
 	}
 
@@ -30,14 +63,13 @@ public class Update extends Action {
 	}
 
 	@Override
-    public String toString() {
-    	String str = this.getSecondToExecute()+" UPDATE "+filePath+" ";
-    	for (ByteRange range : this.modifications) {
-    		str += (range.toString()+" ");
-    	}
-    	str += "\n";
-    	
-    	return str;
-    }
+	public String toString() {
+		String str = this.getSecondToExecute() + " UPDATE " + filePath + " ";
+		for (ByteRange range : this.modifications) {
+			str += (range.toString() + " ");
+		}
+		str += "\n";
 
+		return str;
+	}
 }
