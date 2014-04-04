bbe7cb4e87054b5fafb275a0f99c34c22b2d52f2
Cristian Cotes <cristian.cotes@urv.cat>
2014-04-04 10:40:39 +0200

    Realistic file size.

--- test/com/ast/dataset/RealisticDatasetTest.java
+++ test/com/ast/dataset/RealisticDatasetTest.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import com.ast.dataset.generator.RealisticDatasetGenerator;
 import com.ast.dataset.models.SimpleFile;
 import com.ast.dataset.util.Config;
+import com.ast.dataset.util.Utils;
 
 public class RealisticDatasetTest {
 
@@ -25,17 +26,69 @@ public class RealisticDatasetTest {
 	public static List<SimpleFile> getSimpleFilesList() {
 		
 		List<SimpleFile> files = new ArrayList<SimpleFile>();
-		Random random = new Random();
 		int i = 0;
 		while (i < 1000) {
-			int size = random.nextInt(4096);
 			String filename = RandomStringUtils.randomAlphabetic(7) + ".gz";
-			SimpleFile file = new SimpleFile(filename, size + 512);
+			SimpleFile file = new SimpleFile(filename, getFileSize());
 			files.add(file);
 			i++;
 		}
 		
 		return files;
+	}
+	
+	public static int getFileSize() {
+		
+		Random random = new Random();
+		int value = random.nextInt(100);
+		
+		int minSize=0, maxSize=0;	// In bytes
+		if (Utils.isBetween(value, 0, 40)) {
+			minSize = 1;
+			maxSize = 4*1024;
+		} else if (Utils.isBetween(value, 40, 50)) {
+			minSize = 4*1024;
+			maxSize = 8*1024;
+		} else if (Utils.isBetween(value, 50, 58)) {
+			minSize = 8*1024;
+			maxSize = 16*1024;
+		} else if (Utils.isBetween(value, 58, 66)) {
+			minSize = 16*1024;
+			maxSize = 32*1024;
+		} else if (Utils.isBetween(value, 66, 72)) {
+			minSize = 32*1024;
+			maxSize = 64*1024;
+		} else if (Utils.isBetween(value, 72, 78)) {
+			minSize = 64*1024;
+			maxSize = 128*1024;
+		} else if (Utils.isBetween(value, 78, 81)) {
+			minSize = 128*1024;
+			maxSize = 256*1024;
+		} else if (Utils.isBetween(value, 81, 87)) {
+			minSize = 256*1024;
+			maxSize = 512*1024;
+		} else if (Utils.isBetween(value, 87, 90)) {
+			minSize = 512*1024;
+			maxSize = 1024*1024;
+		} else if (Utils.isBetween(value, 90, 94)) {
+			minSize = 1024*1024;
+			maxSize = 2048*1024;
+		} else if (Utils.isBetween(value, 94, 97)) {
+			minSize = 2048*1024;
+			maxSize = 4096*1024;
+		} else if (Utils.isBetween(value, 97, 98)) {
+			minSize = 4096*1024;
+			maxSize = 8192*1024;
+		} else if (Utils.isBetween(value, 98, 99)) {
+			minSize = 8192*1024;
+			maxSize = 16384*1024;
+		} else if (Utils.isBetween(value, 99, 100)) {
+			minSize = 16384*1024;
+			maxSize = 262144*1024;
+		} 
+		
+		int size = minSize + random.nextInt(maxSize-minSize+1);
+		return size;
 	}
 
 }
